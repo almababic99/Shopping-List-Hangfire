cccd12bac6aa08b20e0cc4f89815b75b8609f03f
Alma Babic <alma.babic@tacta.io>
2025-01-28 09:57:12 +0100

    Shopping List

---
+++ backend/Application/Commands/CreateItemCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using Application.Interfaces;
+using Domain.DomainModels;
+using MediatR;
+
+namespace Application.Commands
+{
+    public class CreateItemCommandHandler : IRequestHandler<CreateItemCommand>
+    {
+        private readonly IItemRepository _itemRepository;
+        private readonly IEndpointInstance _endpointInstance;
+
+        public CreateItemCommandHandler(IItemRepository itemRepository, IEndpointInstance endpointInstance)
+        {
+            _itemRepository = itemRepository;
+            _endpointInstance = endpointInstance;
+        }
+
+        public async Task Handle(CreateItemCommand request, CancellationToken cancellationToken)
+        {
+            var item = new Item { Id = request.Id, Name = request.Name };  // mapping dto to domain
+
+            var existingItem = await _itemRepository.GetItem(item.Name);
+
+            if (existingItem != null)
+            {
+                throw new InvalidOperationException("An item with the same name already exists");  // if the name already exists it throws an error
+            }
+
+            await _itemRepository.AddItem(item).ConfigureAwait(false);
+
+            var message = new ItemAddedMessage
+            {
+                Message = "New item is added!"
+            };
+
+            Console.WriteLine($"Sending message: {message.Message}");
+            await _endpointInstance.Send("ServiceBus", message);
+        }
+    }
+}
---
+++ backend/Application/Commands/CreateShopperCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using Application.Interfaces;
+using Domain.DomainModels;
+using MediatR;
+
+namespace Application.Commands
+{
+    public class CreateShopperCommandHandler : IRequestHandler<CreateShopperCommand>
+    {
+        private readonly IShopperRepository _shopperRepository;
+        private readonly IEndpointInstance _endpointInstance;
+
+        public CreateShopperCommandHandler(IShopperRepository shopperRepository, IEndpointInstance endpointInstance)
+        {
+            _shopperRepository = shopperRepository;
+            _endpointInstance = endpointInstance;
+        }
+
+        public async Task Handle(CreateShopperCommand request, CancellationToken cancellationToken)
+        {
+            var shopper = new Shopper { Id = request.Id, Name = request.Name };   // mapping dto to domain
+
+            var existingShopper = await _shopperRepository.GetShopper(shopper.Name);
+
+            if (existingShopper != null)
+            {
+                throw new InvalidOperationException("Shopper with the same name already exists");  // if the name already exists it throws an error
+            }
+
+            await _shopperRepository.AddShopper(shopper);
+
+            var message = new ShopperAddedMessage
+            {
+                Message = "New shopper is added!"
+            };
+
+            Console.WriteLine($"Sending message: {message.Message}");
+            await _endpointInstance.Send("ServiceBus", message);
+        }
+    }
+}
